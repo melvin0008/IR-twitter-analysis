80fa1a2100f2c0c951091844acaf583b17a904e7
Melvin <melvin0008@gmail.com>
2015-09-15 00:49:14 -0400

    changed for tweets based on us open

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
 import tweepy
+import config
 
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
+auth = tweepy.OAuthHandler(config.CONSUMER_KEY, config.CONSUMER_SECRET)
+auth.set_access_token(config.ACCESS_TOKEN, config.ACCESS_TOKEN_SECRET)
 
 api = tweepy.API(auth)
 
-public_tweets = api.home_timeline()
-for tweet in public_tweets:
-    print tweet.text
+query = 'USOpen'
+lang="en"
+max_tweets = 200
+
+searched_tweets = [status for status in tweepy.Cursor(api.search, q=query,lang=lang).items(max_tweets)]
+
+for tweet in searched_tweets:
+	print tweet.text.encode('utf-8').strip()
