b5393bfe95536a8c06c36b284d22d6a09025b7f0
Melvin <melvin0008@gmail.com>
2015-09-23 11:40:07 -0400

    final push

--- count.py
+++ count.py
@@ -1,16 +1,21 @@
 import codecs
 import json
 # Fri Sep 18 18:05:55 +0000 2015
+import random
 
 from datetime import datetime
 # docs=['tweets1','tweets2','tweets3','tweets4','tweets5']
 count_dic={'ru':0,'de':0,'en':0}
+
+a=100000000000000000
+b=999999999999999999
 
 # for doc in docs:
 with codecs.open("alltweets.json", "r") as stream:
 	data=stream.read()
 	jdata=json.loads(data)
 	for tweet in jdata:
+		tweet['id']=str(random.randint(a, b))
 		fmt = '%Y-%m-%dT%H:%M:%SZ'
 		temp = datetime.strptime(str(tweet['created_at']),'%a %b %d %H:%M:%S %z %Y')
 		tweet['created_at'] = str(temp.strftime(fmt))
